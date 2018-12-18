59720a85e60b93c2031ea6e992e103009263976d
MrHurniak <MrHurniak@gmail.com>
2018-12-19 00:58:31 +0200

    Some changes
    Change duration of delay between messages

--- src/Test.java
+++ src/Test.java
@@ -2,14 +2,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Test {
-    public static void main(String[] args) throws AWTException {
+    public static void main(String[] args) throws AWTException, InterruptedException {
         if(SystemTray.isSupported()){
             SystemTray tray = SystemTray.getSystemTray();
             Image image = new ImageIcon("world.png").getImage();
             TrayIcon trayIcon = new TrayIcon(image,"MyTest",createPopupMenu());
             tray.add(trayIcon);
-            trayIcon.displayMessage("Hi!","Today is a very beautiful day",TrayIcon.MessageType.NONE);
-
+            while (true){
+                trayIcon.displayMessage("Hi!","Today is a very beautiful day",TrayIcon.MessageType.NONE);
+                
+                //I want to display message every 3 hours
+                Thread.sleep(10800000 );
+            }
         } else{
             JOptionPane.showMessageDialog(null,"Your system does not support System Tray");
         }
