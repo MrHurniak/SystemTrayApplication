d52616dceaeb059bb7e032b5935b85f4edd6a5e6
MrHurniak <MrHurniak@gmail.com>
2018-12-19 00:50:17 +0200

    First steps to realisation of interesting project

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args) throws AWTException {
+        if(SystemTray.isSupported()){
+            SystemTray tray = SystemTray.getSystemTray();
+            Image image = new ImageIcon("world.png").getImage();
+            TrayIcon trayIcon = new TrayIcon(image,"MyTest",createPopupMenu());
+            tray.add(trayIcon);
+            trayIcon.displayMessage("Hi!","Today is a very beautiful day",TrayIcon.MessageType.NONE);
+
+        } else{
+            JOptionPane.showMessageDialog(null,"Your system does not support System Tray");
+        }
+    }
+    static private PopupMenu createPopupMenu(){
+        PopupMenu menu = new PopupMenu();
+        MenuItem exit = new MenuItem();
+        exit.setLabel("Close");
+        menu.add(exit);
+        menu.addActionListener((e)->System.exit(0));
+        return menu;
     }
 }
